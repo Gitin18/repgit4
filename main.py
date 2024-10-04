f9303859236fd1f11bc3a5cbbc258913c1f3773c
Gitin18 <inka25618@gmail.com>
2024-10-04 23:30:48 +0200

    add consts

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+pygame = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Game Tir")
+icon = pygame.image.load("")
 
 running = True
 while running:
